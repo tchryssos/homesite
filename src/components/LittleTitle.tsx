d8ed5f411bdcd7c19115d95d6c3e004a9e31e81e
Troy Chryssos <troychryssos@gmail.com>
2020-09-08 10:59:19 -0400

    little title tsx

--- src/components/LittleTitle.tsx
+++ src/components/LittleTitle.tsx
@@ -4,15 +4,22 @@ import { createUseStyles } from 'react-jss'
 
 import { smFont } from 'constants/styles/fonts'
 
+interface Props {
+	className: string,
+	children: React.ReactNode,
+}
+
 const useStyles = createUseStyles({
 	h3: {
 		...smFont,
 	},
 })
 
-export default ({ children, className }) => {
+const LittleTitle: React.FC<Props> = ({ children, className }: Props) => {
 	const classes = useStyles()
 	return (
 		<h3 className={clsx(classes.h3, className)}>{children}</h3>
 	)
 }
+
+export default LittleTitle
