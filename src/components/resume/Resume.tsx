cf53b82753ab3f78f91c26407d57ab3746b4b198
Troy Chryssos <troy@codecademy.com>
2021-07-03 22:16:55 -0400

    prettier resume

--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -1,86 +1,88 @@
-import React from 'react'
-import { createUseStyles } from 'react-jss'
-import clsx from 'clsx'
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+import clsx from 'clsx';
 
-import LittleTitle from 'components/typography/LittleTitle'
-import ExtLink from 'components/ExtLink'
+import LittleTitle from 'components/typography/LittleTitle';
+import ExtLink from 'components/ExtLink';
 
-import resumePdf from 'static/chryssos_resume.pdf'
+import resumePdf from 'static/chryssos_resume.pdf';
 
-import { white, dimmed } from 'constants/styles/colors'
-import { SM_MIN_STRING } from 'constants/styles/breakpoints'
-import { useShadowStyles } from 'logic/util/styles/shadow'
+import { white, dimmed } from 'constants/styles/colors';
+import { SM_MIN_STRING } from 'constants/styles/breakpoints';
+import { useShadowStyles } from 'logic/util/styles/shadow';
 
 const useStyles = createUseStyles({
-	download: {
-		border: [[2, 'solid', white]],
-		borderRadius: 4,
-		padding: [[8, 16]],
-		width: '100%',
-		marginBottom: 24,
-		backgroundColor: dimmed,
-		textAlign: 'center',
-		[SM_MIN_STRING]: {
-			width: 'fit-content',
-		},
-	},
-	downloadText: {
-		color: 'inherit',
-	},
-	resumeContainer: {
-		border: [[2, 'solid', white]],
-		borderRadius: 4,
-		padding: 16,
-		marginBottom: 16,
-		backgroundColor: dimmed,
-		// backgroundColor: '#084C61',
-		display: 'flex',
-		flexWrap: 'wrap',
-	},
-	main: {
-		display: 'flex',
-		flexDirection: 'column',
-		width: '100%',
-		marginBottom: 16,
-		[SM_MIN_STRING]: {
-			width: '80%',
-			marginBottom: 0,
-		},
-	},
-	sidebar: {
-		width: '100%',
-		display: 'flex',
-		flexDirection: 'column',
-		[SM_MIN_STRING]: {
-			width: '20%',
-			textAlign: 'right',
-		},
-	},
-	sectionLabel: {
-		marginBottom: 16,
-		textDecoration: 'underline',
-	},
-})
+  download: {
+    border: [[2, 'solid', white]],
+    borderRadius: 4,
+    padding: [[8, 16]],
+    width: '100%',
+    marginBottom: 24,
+    backgroundColor: dimmed,
+    textAlign: 'center',
+    [SM_MIN_STRING]: {
+      width: 'fit-content',
+    },
+  },
+  downloadText: {
+    color: 'inherit',
+  },
+  resumeContainer: {
+    border: [[2, 'solid', white]],
+    borderRadius: 4,
+    padding: 16,
+    marginBottom: 16,
+    backgroundColor: dimmed,
+    // backgroundColor: '#084C61',
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    marginBottom: 16,
+    [SM_MIN_STRING]: {
+      width: '80%',
+      marginBottom: 0,
+    },
+  },
+  sidebar: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    [SM_MIN_STRING]: {
+      width: '20%',
+      textAlign: 'right',
+    },
+  },
+  sectionLabel: {
+    marginBottom: 16,
+    textDecoration: 'underline',
+  },
+});
 
 const Resume = () => {
-	const classes = useStyles()
-	const shadowClasses = useShadowStyles()
-	return (
-		<>
-			<ExtLink
-				to={resumePdf}
-				download
-				className={clsx(
-					classes.download,
-					shadowClasses.shadow,
-					shadowClasses.hoverShadow,
-				)}
-			>
-				<LittleTitle className={classes.downloadText}>Download Full Resume</LittleTitle>
-			</ExtLink>
+  const classes = useStyles();
+  const shadowClasses = useShadowStyles();
+  return (
+    <>
+      <ExtLink
+        className={clsx(
+          classes.download,
+          shadowClasses.shadow,
+          shadowClasses.hoverShadow,
+        )}
+        download
+        to={resumePdf}
+      >
+        <LittleTitle className={classes.downloadText}>
+          Download Full Resume
+        </LittleTitle>
+      </ExtLink>
 
-			{/* Disabling non-downloaded resume for now since I have a job */}
-			{/* <div className={classes.resumeContainer}>
+      {/* Disabling non-downloaded resume for now since I have a job */}
+      {/* <div className={classes.resumeContainer}>
 				<div className={classes.main}>
 					<LittleTitle className={classes.sectionLabel}>Experience</LittleTitle>
 					{experience.map(
@@ -105,8 +107,8 @@ const Resume = () => {
 					)}
 				</div>
 			</div> */}
-		</>
-	)
-}
+    </>
+  );
+};
 
-export default Resume
+export default Resume;
--- src/components/resume/ResumeExperience.tsx
+++ src/components/resume/ResumeExperience.tsx
@@ -1,48 +1,49 @@
-import React from 'react'
-import { createUseStyles } from 'react-jss'
+import React from 'react';
+import { createUseStyles } from 'react-jss';
 
-import SubText from 'components/typography/SubText'
-import LittleTitle from 'components/typography/LittleTitle'
+import SubText from 'components/typography/SubText';
+import LittleTitle from 'components/typography/LittleTitle';
 
-import { ExperienceProps } from 'constants/resume'
+import { ExperienceProps } from 'constants/resume';
 
 const useStyles = createUseStyles({
-	experienceContainer: {
-		display: 'flex',
-		flexDirection: 'column',
-		marginBottom: 16,
-		'&:last-child': {
-			marginBottom: 0,
-		},
-	},
-	dates: {
-		marginBottom: 8,
-		color: 'rgba(255, 255, 255, 0.7)',
-	},
-})
+  experienceContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: 16,
+    '&:last-child': {
+      marginBottom: 0,
+    },
+  },
+  dates: {
+    marginBottom: 8,
+    color: 'rgba(255, 255, 255, 0.7)',
+  },
+});
 
 const ResumeExperience: React.FC<ExperienceProps> = ({
-	company, title, dates, highlights,
+  company,
+  title,
+  dates,
+  highlights,
 }) => {
-	const classes = useStyles()
-	return (
-		<div className={classes.experienceContainer}>
-			<LittleTitle>
-				{company}
-				&nbsp;-&nbsp;
-				{title}
-			</LittleTitle>
-			<SubText className={classes.dates}>{dates}</SubText>
-			{highlights.map(
-				(h) => (
-					<SubText key={h}>
-						+&nbsp;
-						{h}
-					</SubText>
-				),
-			)}
-		</div>
-	)
-}
+  const classes = useStyles();
+  return (
+    <div className={classes.experienceContainer}>
+      <LittleTitle>
+        {company}
+        &nbsp;-&nbsp;
+        {title}
+      </LittleTitle>
+      <SubText className={classes.dates}>{dates}</SubText>
+      {highlights.map((h) => (
+        <SubText key={h}>
+          +&nbsp;
+          {h}
+        </SubText>
+      ))}
+    </div>
+  );
+};
 
-export default ResumeExperience
+export default ResumeExperience;
