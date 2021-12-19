694cbe0fcb2d0d085d0f217d3e97f9633efe816f
Troy Chryssos <troy@codecademy.com>
2021-12-19 11:40:33 -0500

    more config to stop 404s

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ module.exports = {
   eslint: {
     dirs: ['pages', 'utils', 'constants', 'components', 'typings'],
   },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
   images: {
     domains: ['upload.wikimedia.org'],
   },
