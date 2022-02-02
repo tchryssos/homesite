2015b3463f77053685554a777b1256d0dae3eea1
Troy Chryssos <troychryssos@gmail.com>
2022-02-01 22:53:03 -0500

    moving pixel shifter to new component

---
+++ src/components/PixelContentShifter.tsx
@@ -0,0 +1,82 @@
+import { keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import { PAGE_TRANSITION_TIME } from '~/constants/animation';
+import { ABOUT_PATH, HOME_PATH } from '~/constants/routes';
+import { PIXEL_CONTENT_HEIGHT } from '~/constants/style';
+import { pxToRem } from '~/logic/util/styles/pxToRem';
+
+import { FlexBox } from './box/FlexBox';
+
+interface ShifterProps {
+  isAboutPage: boolean;
+  windowHeight: number;
+  navHeight: number;
+  lastRoute: string | undefined;
+}
+
+export const PixelContentShifter = styled(FlexBox)<ShifterProps>(
+  ({ isAboutPage, windowHeight, navHeight, lastRoute, theme }) => {
+    const translateDown = `translateY(${pxToRem(
+      windowHeight - navHeight - PIXEL_CONTENT_HEIGHT,
+    )})`;
+
+    const pixelSlideDown = keyframes`
+      from {
+        transform: translateY(0)
+      }
+      to {
+        transform: ${translateDown}
+      }
+    `;
+
+    const pixelWrapAround = keyframes`
+      from {
+        transform: ${translateDown}
+        visibility: visible;
+      }
+      50% {
+        visibility: visible;
+        transform: translateY(100vh)
+      }
+      51% {
+        visibility: hidden;
+        transform: translateY(${pxToRem(
+          (navHeight + PIXEL_CONTENT_HEIGHT) * -1,
+        )})
+        52% {
+          visibility: visible;
+        }
+        100% {
+          transform: translateY(0);
+        }
+      }
+
+    `;
+
+    const aboutPageStyles = {
+      width: '100%',
+      position: 'fixed',
+      ...(lastRoute === HOME_PATH
+        ? {
+            animation: `${pixelSlideDown} ${PAGE_TRANSITION_TIME}ms linear forwards`,
+          }
+        : {
+            transform: translateDown,
+          }),
+    } as Record<string, unknown>;
+
+    const homePageStyles = {
+      ...(lastRoute === ABOUT_PATH && {
+        animation: `${pixelWrapAround} ${PAGE_TRANSITION_TIME}ms linear forwards`,
+      }),
+    } as Record<string, unknown>;
+
+    return {
+      zIndex: 2,
+      height: '100%',
+      backgroundImage: `linear-gradient(to bottom, transparent, ${theme.colors.primary} 10%)`,
+      ...(isAboutPage ? aboutPageStyles : homePageStyles),
+    };
+  },
+);
--- src/components/meta/Layout.tsx
+++ src/components/meta/Layout.tsx
@@ -1,79 +1,20 @@
-import { keyframes } from '@emotion/react';
-import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
 import { useContext, useRef } from 'react';
 
-import { PAGE_TRANSITION_TIME } from '~/constants/animation';
-import { ABOUT_PATH, HOME_PATH } from '~/constants/routes';
-import { PIXEL_CONTENT_HEIGHT } from '~/constants/style';
-import { AnimationContext } from '~/logic/contexts/animation';
+import { ABOUT_PATH } from '~/constants/routes';
 import { HistoryContext } from '~/logic/contexts/history';
-import { pxToRem } from '~/logic/util/styles/pxToRem';
 import { getWindow } from '~/logic/util/window';
 
-import { FlexBox } from '../box/FlexBox';
 import { NavBar } from '../NavBar';
 import { PageWrapper } from '../PageWrapper';
 import { PixelContent } from '../PixelContent';
+import { PixelContentShifter } from '../PixelContentShifter';
 import { Head } from './Head';
 
 type LayoutProps = {
   children?: React.ReactNode;
   title?: string;
 };
-
-interface ShifterProps {
-  isAboutPage: boolean;
-  windowHeight: number;
-  navHeight: number;
-  lastRoute: string | undefined;
-  isAnimating: boolean;
-}
-
-const PixelShifter = styled(FlexBox)<ShifterProps>(
-  ({ isAboutPage, windowHeight, navHeight, lastRoute, theme }) => {
-    const translateDown = `translateY(${pxToRem(
-      windowHeight - navHeight - PIXEL_CONTENT_HEIGHT,
-    )})`;
-
-    const pixelSlideDown = keyframes`
-    from {
-      transform: translateY(0)
-    }
-    to {
-      transform: ${translateDown}
-    }
-  `;
-
-    const slidingAnimation = `${pixelSlideDown} ${PAGE_TRANSITION_TIME}ms linear`;
-
-    const aboutPageStyles = {
-      width: '100%',
-      position: 'fixed',
-      zIndex: 2,
-      ...(lastRoute === HOME_PATH
-        ? {
-            animation: `${slidingAnimation} forwards`,
-          }
-        : {
-            transform: translateDown,
-          }),
-    } as Record<string, unknown>;
-
-    const homePageStyles = {
-      ...(lastRoute === ABOUT_PATH && {
-        animation: `${slidingAnimation} reverse`,
-      }),
-    } as Record<string, unknown>;
-
-    return {
-      height: '100%',
-      backgroundImage: `linear-gradient(to bottom, transparent, ${theme.colors.primary} 10%)`,
-      ...(isAboutPage ? aboutPageStyles : homePageStyles),
-    };
-  },
-);
-
 interface PixelSectionProps {
   navHeight: number;
 }
@@ -81,19 +22,17 @@ interface PixelSectionProps {
 const PixelSection: React.FC<PixelSectionProps> = ({ navHeight }) => {
   const { pathname } = useRouter();
   const [, lastRoute] = useContext(HistoryContext);
-  const { isAnimating } = useContext(AnimationContext);
   const windowHeight = getWindow()?.innerHeight || 0;
 
   return (
-    <PixelShifter
+    <PixelContentShifter
       isAboutPage={pathname === ABOUT_PATH}
-      isAnimating={isAnimating}
       lastRoute={lastRoute}
       navHeight={navHeight}
       windowHeight={windowHeight}
     >
       <PixelContent />
-    </PixelShifter>
+    </PixelContentShifter>
   );
 };
 
