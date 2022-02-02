ce694a99bcb790e39d407f2af61b27982e2391c9
Troy Chryssos <troychryssos@gmail.com>
2022-02-01 23:17:31 -0500

    hide home while animating

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
+import { useContext } from 'react';
+
 import { Layout } from '~/components/meta/Layout';
 import { CurrentGig } from '~/components/portfolio/CurrentGig';
 import { Experiments } from '~/components/portfolio/Experiments';
 import { Other } from '~/components/portfolio/Other';
 import { Work } from '~/components/portfolio/Work';
+import { AnimationContext } from '~/logic/contexts/animation';
 
-const Portfolio = () => (
-  <Layout>
-    <CurrentGig />
-    <Experiments />
-    <Work />
-    <Other />
-  </Layout>
-);
+const Portfolio = () => {
+  const { isAnimating } = useContext(AnimationContext);
+
+  return (
+    <Layout>
+      {!isAnimating && (
+        <>
+          <CurrentGig />
+          <Experiments />
+          <Work />
+          <Other />
+        </>
+      )}
+    </Layout>
+  );
+};
 
 export default Portfolio;
