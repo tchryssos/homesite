0750f2d144059f0a3068f57ff581aafc64944cd5
Troy Chryssos <troychryssos@gmail.com>
2025-02-22 12:19:17 -0800

    funky sake block, remove moss, update codecademy text

--- src/components/portfolio/Work.tsx
+++ src/components/portfolio/Work.tsx
@@ -5,19 +5,19 @@ export function Work() {
   return (
     <PortfolioSection desc="A selection of professional projects" title="Work">
       <ProjectBlock
+        imageAltText="Funky Sake's face"
+        imageSrc="/funky-face.png"
+        text="Website and blog for sake sommalier and influencer Funky Sake."
+        title="Funky Sake"
+        to="https://www.funkysake.com"
+      />
+      <ProjectBlock
         imageAltText="Sparkle emoji"
         imageSrc="/sparkle.png"
         text="Portfolio website for graphic &amp; UX designer Casey Bradford."
         title="casey&#x0200B;bradford&#x0200B;.club"
         to="https://caseybradford.club/"
         toRepo="https://github.com/hottinet/casey-site-v2"
-      />
-      <ProjectBlock
-        imageAltText="Moss sprout logo"
-        imageSrc="/moss.png"
-        text="A shared virtual desktop workspace for creative teams to view and organize project assets."
-        title="Moss Workspace"
-        to="https://mossworkspace.com/"
       />
       <ProjectBlock
         hideLink
